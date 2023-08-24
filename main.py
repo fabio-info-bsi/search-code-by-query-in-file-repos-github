cc6eae58911e509e1732e586f9793f9cdb91370e
fabio.henrique <fabio.henrique@hotmart.com>
2023-08-24 14:20:51 -0300

    feat: parametrizations; requirements.txt and .env

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os
 import subprocess
+from decouple import config
 from time import sleep
 
-words_txt = 'words.txt'
+INPUT_TXT = config('INPUT_TXT')
+OWNER_REPO_GIT = config('OWNER_REPO_GIT')
 
 
-def read_txt(arquivo):
-    with open(arquivo) as f:
-        for line in f:
-            yield line
+def read_txt(file):
+    with open(file) as f:
+        for lineFile in f:
+            yield lineFile
 
 
 def save_file(file_name, identification, data):
@@ -17,20 +18,21 @@ def save_file(file_name, identification, data):
 
 
 if __name__ == "__main__":
-    #os.system("gh auth login")
+    # os.system("gh auth login")
     foundRepos = []
-    for line in read_txt(words_txt):
+    for line in read_txt(INPUT_TXT):
         try:
             query = line.replace('\n', '')
-            proc = subprocess.Popen([f"gh search code {query} --owner=Hotmart-Org"],
+            proc = subprocess.Popen([f"gh search code {query} --owner={OWNER_REPO_GIT}"],
                                     stdout=subprocess.PIPE, shell=True)
             (out, err) = proc.communicate()
             repos = str(out.decode("utf-8")).split('\n')
             if len(repos) > 1:
                 for repo in repos:
                     if len(str(repo)) > 1:
-                        print(repo)
-                        filtered = list(filter(lambda _obj: _obj['repo'] == str(repo).split(':')[0], foundRepos))
+                        # print(repo)
+                        filtered = list(filter(lambda _obj: _obj['repo'] == str(repo).split(':')[0]
+                                                            and _obj['query'] == query, foundRepos))
                         if len(filtered) > 0:
                             filtered[0]['fileName'].append(str(repo).split(':')[1])
                         else:
@@ -45,8 +47,8 @@ if __name__ == "__main__":
             print(f"Error: {e} -> " + line)
     print('-------------------------------------------')
     for repo in foundRepos:
-        print("Query = "+repo['query'])
-        print("=> "+repo['repo'])
+        print("Query = " + repo['query'])
+        print("=> " + repo['repo'])
         for file in repo['fileName']:
-            print("     => "+file)
+            print("     => " + file)
         print('-------------------------------------------')
